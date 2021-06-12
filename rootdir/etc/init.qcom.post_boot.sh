5ce9b40c2a0be8cd83b814fc0c27362396984860
D4rkKnight21 <yusuff8621@gmail.com>
2021-06-12 09:59:07 +0700

    sdm660-common: set swappines to 80

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -410,7 +410,7 @@ function enable_swap() {
     if [ "$MemTotal" -le "$SWAP_ENABLE_THRESHOLD" ] && [ "$swap_enable" == "true" ]; then
         # Static swiftness
         echo 1 > /proc/sys/vm/swap_ratio_enable
-        echo 70 > /proc/sys/vm/swap_ratio
+        echo 80 > /proc/sys/vm/swap_ratio
 
         # Swap disk - 200MB size
         if [ ! -f /data/vendor/swap/swapfile ]; then
@@ -449,7 +449,7 @@ if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] || [ "$Produc
       configure_zram_parameters
       configure_read_ahead_kb_values
       echo 0 > /proc/sys/vm/page-cluster
-      echo 100 > /proc/sys/vm/swappiness
+      echo 80 > /proc/sys/vm/swappiness
 else
     arch_type=`uname -m`
 
@@ -541,7 +541,7 @@ else
     # Set allocstall_threshold to 0 for all targets.
     # Set swappiness to 100 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
+    echo 80 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
